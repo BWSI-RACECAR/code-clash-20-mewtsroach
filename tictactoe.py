f57def5d859cac44251872143a364e988e53148d
mewtsroach <102555692+mewtsroach@users.noreply.github.com>
2022-08-05 07:11:18 -0700

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -38,7 +38,7 @@ class Solution:
         # type a: int
         # type b: int
         # return type: int
-        pos_cases = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 9], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+        pos_cases = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
         for i in pos_cases:
             if a in i and b in i:
                 i.remove(a)
